279238cac5ebac4a6f2f77756375454407d3d15d
JosephSAlemu <jojoalemu@gmail.com>
2025-02-17 14:33:23 -0600

    added to filter and api files

--- src/api.py
+++ src/api.py
@@ -4,26 +4,87 @@ import json
 import csv
 from filter import filter_data
 
+
+
+voxel = 0
+
+headers = [
+"X","Y","Z"
+]
+
 def api_calls() -> None:
     ...
 
-def structure_to_image():
-    ...
+
+
+def structure_to_image() -> None:
+    file = pd.read_csv(r'C:\Users\jojoa\Downloads\Motorola_Research\ExcelScript\Datasets\Outputs\NewDenC_Microns.csv')
+
+    #max = len(file['X'])
+    start = 0
+    while start < 1:
+        chunk(file['X'][start], file['Y'][start], file['Z'][start])
+        start+=1
+        print(f"\n{voxel}")
+   
+def chunk(X: int, Y: int, Z: int ) -> None:
+    global voxel
+    list = []
+    chunk_headers = [
+        "Voxel", "Section Image"
+    ]
+    created_file = r"./Datasets/Outputs/P4_New_Chunk.csv"
+
+    with open(r'./Datasets/Inputs/section_dataset_ids_reference_6_sagittal.txt') as file, open(created_file, mode ="a", newline="") as new_file:
+        writer = csv.writer(new_file)
+        writer.writerow(chunk_headers)
+        
+        for line in file:
+            if len(list) == 100:
+                url = f"http://api.brain-map.org/api/v2/reference_to_image/10.json?x={X}&y={Y}&z={Z}&section_data_set_ids={','.join(map(str, list))}"
+                response = requests.get(url)
+                data = f"{response.json()['msg']}"
+                writer.writerow([voxel, data])
+                list = []
+            else:
+                list.append(int(line))
+        url = f"http://api.brain-map.org/api/v2/reference_to_image/10.json?x={X}&y={Y}&z={Z}&section_data_set_ids={','.join(map(str, list))}"
+        response = requests.get(url)
+        data = f"{response.json()}"
+        writer.writerow([voxel, data])
+        list = []
+        
+        voxel+=1 
+
+
+
+
+
+def multiply_coordinate_for_microns() -> None:
+    """
+    takes all the p-56 voxel coordinates and converts them to microns by multiplying them by 200
+
+    Then, it runs it through the API
+
+    """
+    file = pd.read_csv(r'C:\Users\jojoa\Downloads\Motorola_Research\ExcelScript\Datasets\Inputs\NewDenC.csv')
+    file['X'] = file['X'] * 200
+    file['Y'] = file['Y'] * 200
+    file['Z'] = file['Z'] * 200
     
+    file.to_csv(r'C:\Users\jojoa\Downloads\Motorola_Research\ExcelScript\Datasets\Outputs\NewDenC_Microns.csv', index = False)
+
 def image_to_structure(p5_voxel: int) -> None:
     """
     takes all the p-4 section images associated with a p-56 voxels [1465 voxels]
 
-    p5_voxel values: 0..1464
+    p5_voxel values: 0 to 1464 (inclusive)
 
     """
-    
+
     data = filter_data(p5_voxel)
-    print(len(data))
-    headers = [
-        "X","Y","Z"
-    ]
-    file = f"./result{p5_voxel}.csv"
+    #r"./Datasets/Outputs
+    file = f"./Datasets/Outputs/New_result{p5_voxel}.csv"
     with open(file, mode ="a", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(headers)
@@ -39,17 +100,9 @@ def image_to_structure(p5_voxel: int) -> None:
                 data = data.replace("True", "true")
                 data = json.loads(data)
                 voxel = data['msg']['image_to_reference']
-                x,y,z = voxel['x'], voxel['y'], voxel['z']
+                x,y,z = voxel['x']/160, voxel['y']/160, voxel['z']/160
                 writer.writerow([x, y, z])
             else:
                 print(f"Failed to fetch data: {response.status_code}")
 
-
-image_to_structure(2)
-image_to_structure(3)
-image_to_structure(4)
-image_to_structure(5)
-image_to_structure(6)
-image_to_structure(7)
-image_to_structure(8)
-image_to_structure(9)
+structure_to_image()
--- src/filter.py
+++ src/filter.py
@@ -1,8 +1,18 @@
 import pandas as pd
 
 import json
+
+
+# filters out the p4 complete brain (structures and genes) and gets only the brainstem
+def filter_p4_data() -> None:
+    fil = pd.read_csv(r'C:\Users\jojoa\Downloads\Motorola_Research\ExcelScript\Datasets\Outputs\P4_Complete_Brain.csv')
+    answer = fil.loc[fil['structure_id'] > 17290]
+    answer.to_csv("./Datasets/Outputs/P4_Complete_Brainstem.csv", index=False)
+    with open(r'./result.txt', mode ="w") as ff:
+        ff.write(str(answer.shape[0]))
+
 def get_dataset_id() -> None:
-    dataset = pd.read_csv("Datasets\Inputs\P4_section_images_cleaned.csv")
+    dataset = pd.read_csv(r"Datasets\Inputs\P4_section_images_cleaned.csv")
     temp = dataset["Section Image"]
     res = set()
     for j in temp:
@@ -16,12 +26,12 @@ def get_dataset_id() -> None:
     with open(r"./result.txt", mode="w") as ff:
         ff.write('\n'.join(str(i) for i in res))
 
-# Take the dataset, filter out the id, x, and y -> put through the image to reference API.
-# Pray the voxels in the reference space are touching each other for each chunk.
-# If they do touch each other, then we can just use one image per chunk lowering are computational cost by a lot.
+
 def filter_data(voxel: int) -> list[dict]:
-    # Input the path of the csv file with the 21 chunks and ten p-56 voxels.
-    dataset = pd.read_csv("[Insert_File_Path]")
+    """
+    Retrieves a List of corresponding Section Images for the p_4 mouse for the corresponding p-56 voxel inputted
+    """
+    dataset = pd.read_csv(r"C:\Users\jojoa\Downloads\Motorola_Research\ExcelScript\Datasets\Outputs\P4_New_Chunk.csv")
     temp = dataset["Section Image"]
     offset = voxel * 21
     end = offset + 20
@@ -39,10 +49,105 @@ def filter_data(voxel: int) -> list[dict]:
 
     return result
         
-        
+def check_length() -> None:
+    mouse = pd.read_csv(r"./Datasets/Outputs/NewDenS_COMB_Genes.csv")
+    print(len(mouse.columns))
+
+
+def find_missing_genes():
+    with open(r"./Datasets/Outputs/P56_UniqueGenes.txt", "r") as p56_genes, open(r"./Datasets/Outputs/P4_UniqueGenes.txt", "r") as p4_genes:
+        p56 = [i[:-1] for i in p56_genes]
+        p4 = [i[:-1] for i in p4_genes]
+        with open(r"./Datasets/Outputs/P4_Exclusive_Genes.txt", "w") as file:
+            for gene in p4:
+                if gene not in p56:
+                    file.write(str(gene) + "\n")
+
+
+def get_unique_genes(common_genes: list[str]) -> list[str]:
+    genes = []
+    experiment = []
+    for gene in common_genes:
+        col = gene.rsplit("-", 1)
+        if len(col) == 2: 
+            if col[0] in genes:
+                exp_index = genes.index(col[0])
+                if experiment[exp_index] < int(col[1]):
+                    #print(f"replaced {col[0]}-{experiment[exp_index]} with {col[0]}-{int(col[1])} ")
+                    experiment[exp_index] = int(col[1])
+            else:
+                genes.append(col[0])
+                experiment.append(int(col[1]))
+    with open(r"./Datasets/Outputs/P56_UniqueGenes.txt", "w") as file:
+        for gene in genes:
+            file.write(str(gene) + "\n")
+    result = [genes[i] + "-" + str(experiment[i]) for i in range(len(genes))]
+    return result
+
+
+def filter_common_genes() -> None:
+    """
+    Filters out the common genes between a p-56 and p-4 mouse
+
+    Creates a new csv with all the p-56 column data only for genes that exist in both mice
+    """
+    try:
+        p56_file = pd.read_csv(r"./Datasets/Inputs/NewDenS.csv")
+        p4_file = pd.read_csv(r"./Datasets/Outputs/p4_NewDenS.csv")
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+        exit()
+
+    common_genes = []
+    for p56_column in p56_file.columns:
+        temp = p56_column.rsplit("-",1)
+        for p4_column in p4_file.columns:
+            temp2 = p4_column.rsplit("-", 1)
+            if temp[0] == temp2[0]:
+                common_genes.append(p56_column)
+
+    unique_genes = []
+    unique_genes.extend(p56_file.columns[1:6])
+    unique_genes.extend(get_unique_genes(common_genes))
+
+    result = p56_file.filter(items = unique_genes)
+    result.to_csv(r"./Datasets/Outputs/NewDenS_COMB_Genes.csv", index=True)
+
+def noise_threshold() -> None:
+    '''
+    Remove a Column (Gene) if it's values for all rows is below 0.001.
+    '''
+    p56_file = pd.read_csv(r"./Datasets/Outputs/NewDenS_COMB_Genes.csv")
+
+    threshold = 0.001
+    above_threshold = False
+    cols = p56_file.shape[1]
+    rows = p56_file.shape[0]
+    
+
+
+    saved_rows = []
+    saved_rows.extend(p56_file.columns[1:6])
+
+    #start from 6 to end of row length
+    count = 0
+    for col in range(6, cols):
+        gene = p56_file.filter(items = [p56_file.columns[col]])
+        print(count)
+        for row in range(rows):
+            if gene.iloc[row,0] > threshold:
+                above_threshold = True
+        if above_threshold == True:
+            saved_rows.append(p56_file.columns[col])
+            above_threshold = False
+        count+=1
+    
+    result = p56_file.filter(items = saved_rows)
+    print(len(saved_rows))
+    result.to_csv(r"./Datasets/Outputs/Noise_NewDenS_COMB_Genes.csv", index=True)
 
 
 
-def get_unique_structures() -> None:
-    structures = pd.read_csv("Datasets\Outputs\P4_Complete_Brain.csv")
-    print(structures['structure_name'].unique())
+    ...
+
+noise_threshold()
